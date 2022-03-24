4eb50eccd97e46c0bed4d3ce0a96c90fff2425d3
matutosi <matutosi@gmail.com>
2022-03-24 09:53:29 +0900

    remove japanese

--- R/data.R
+++ R/data.R
@@ -1,9 +1,7 @@
-  #' 『吾輩は猫である』の冒頭部分
-  #'
-  #' 『吾輩は猫である』の冒頭部分
-  #'
-  #' @format Tibble
-  #' \describe{
-  #'   \item{text}{本文}
-  #' }
+#' The first part of "I Am a Cat" by Soseki NATSUME
+#'
+#' @format Tibble
+#' \describe{
+#'   \item{text}{Body text.}
+#' }
 "neko"
