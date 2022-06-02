590157e93b861124c2c3aeec29e9272f53dea634
matutosi <matutosi@gmail.com>
2022-06-02 16:58:41 +0900

    rename

--- data-raw/replace_words.R
+++
@@ -1,9 +0,0 @@
-## code to prepare `replace_words` dataset goes here
-
-library(tidyverse)
-replace_words <-
-  readr::read_tsv("tools/replace_words.txt", col_names = TRUE) %>%
-  dplyr::mutate_all(stringi::stri_escape_unicode)
-usethis::use_data(replace_words, overwrite = TRUE)
-usethis::use_data_raw("replace_words")
-
---
+++ data-raw/synonym.R
@@ -0,0 +1,10 @@
+## code to prepare `synonym` dataset goes here
+## code to prepare `replace_words` dataset goes here
+
+library(tidyverse)
+synonym <-
+  readr::read_tsv("tools/synonym.txt", col_names = TRUE) %>%
+  dplyr::mutate_all(stringi::stri_escape_unicode)
+usethis::use_data(synonym, overwrite = TRUE)
+usethis::use_data_raw("synonym")
+
