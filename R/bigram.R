f3b726942ce0f9c656d12302a491474406bf31e4
matutosi <matutosi@gmail.com>
2022-06-02 18:04:31 +0900

    improve

--- R/bigram.R
+++ R/bigram.R
@@ -1,55 +1,62 @@
 bigram <- function(df, text_id = "text_id"){
+  word_1 <- "word_1"
+  word_2 <- "word_2"
+  term <- "term"
+  freq <- "freq"
   df %>%
     dplyr::group_by(.data[[text_id]]) %>%
   # according to arrow direction in ggplot: "word_2-word_1"
     dplyr::transmute(text_id, 
-                     word_2 = term, 
-                     word_1 = dplyr::lag(term), 
-                     bigram = stringr::str_c(word_2, " - ", word_1)) %>%
+                     {{word_2}} := term, 
+                     {{word_1}} := dplyr::lag(.data[[term]])) %>%
+  #                      bigram = stringr::str_c(.data[[word_2]], " - ",.data[[ word_1]])) %>%
     dplyr::ungroup() %>%
     na.omit() %>%
     dplyr::group_by(.data[[word_1]], .data[[word_2]]) %>%
-    dplyr::summarise(freq = dplyr::n()) %>%
+    dplyr::tally(name = {{freq}}) %>%
     dplyr::ungroup() %>%
     dplyr::arrange(dplyr::desc(.data[[freq]]))
 }
 
+  # bigram network
+bigram_net <- function(bigram, rand_seed = 12, threshold = 100){
+  set.seed(rand_seed)
+  bigram %T>%
+    { freq_thresh <<- dplyr::slice(., threshold)$freq } %>%
+    dplyr::filter(freq > freq_thresh) %>%
+    igraph::graph_from_data_frame()
+}
 
-freq_ratio <- function(){
+freq_ratio <- function(df, bigram_net){
+  freq <- "freq"
   term <- 
-    bigram_net() %>%
+    bigram_net %>%
     igraph::V() %>%
-    .$name
-  data_in %>%
-    dplyr::group_by(term) %>%
-    dplyr::tally() %>%
-    dplyr::left_join(tibble::tibble(term = term), .) %>%
-    .$n %>%
+    attr("name")
+  df <- 
+    df %>%
+    dplyr::group_by(.data[["term"]]) %>%
+    dplyr::tally(name = freq )
+  dplyr::left_join(tibble::tibble("term" := term), df) %>%
+    `[[`(freq) %>%
     log() %>%
     round(0) * 2
 }
 
-  # bigram network
-bigram_net <- function(){
-  set.seed(input$rand_seed)
-  threshold <- input$threshold
-  bigram() %T>%
-    { freq_thresh <<- dplyr::slice(., threshold)$freq } %>%
-    dplyr::filter(freq > freq_thresh) %>%
-    graph_from_data_frame()
-}
 
-bigram_network_raw <- function(){
-  req(bigram_net())
-
+bigram_network <- function(bigram_net, freq){
   arrow_size  <- unit(input$arrow_size, 'mm')
+  cap_size    <- circle(input$arrow_size, 'mm')
   circle_size <- input$circle_size
   text_size   <- input$text_size
   font_family <- if(exists("input$font")) input$font else ""
-  bigram_net() %>%
+  arrow_col  <- input$arrow_col
+  ccircle_col <- input$circle_col
+
+  bigram_net %>%
     ggraph(layout = "fr") +        # the most understandable layout
-    geom_edge_link(color  = input$arrow_col,  arrow = arrow(length = arrow_size), start_cap = circle(input$arrow_size, 'mm'), end_cap = circle(input$arrow_size, 'mm')) +
-    geom_node_point(color = input$circle_col, size = freq_ratio() * circle_size * 0.2) +  # default (5) means 5 * 0.2 = 1
+    geom_edge_link(color  = arrow_col,  arrow = arrow(length = arrow_size), start_cap = cap_size, end_cap = ccap_size) +
+    geom_node_point(color = circle_col, size = freq_ratio() * circle_size * 0.2) +  # default (5) means 5 * 0.2 = 1
     geom_node_text(aes(label = name), vjust = 1, hjust = 1, size = text_size, family = font_family) +
     ggplot2::theme_bw() + 
     ggplot2::theme(axis.title.x = element_blank(),
