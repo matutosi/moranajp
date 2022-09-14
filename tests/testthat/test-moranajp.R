50b3d0cb2c06a68310ebc1b67b4834c0a62ee3ec
matutosi <matutosi@gmail.com>
2022-09-15 07:18:39 +0900

    delete unused tests

--- tests/testthat/test-moranajp.R
+++ tests/testthat/test-moranajp.R
@@ -7,7 +7,6 @@ test_that("add_series_no and add_text_id work", {
       # add_series_no()
     expect_equal(add_series_no(tbl, cond=".$col == 'EOS' & !is.na(.$col_na)")$series_no, rep(1, nrow(tbl)))
     expect_equal(add_series_no(tbl, cond=".$col == 'EOS' &  is.na(.$col_na)")$series_no, res)
-    expect_equal(add_series_no(tbl, cond=".$col == 'EOS'", end_sep=FALSE)$series_no, res_false) # when separator indicate start
       # add_text_id()
     expect_equal(add_text_id(tbl)$text_id, res)
 })
@@ -24,26 +23,4 @@ test_that("text_id of moranajp matches text number", {
     }
     skip_if(length(bin_dir) != 1)
         expect_equal(res$cols, res$text_id)
-})
-
-test_that("cmd makes (this command is only for test)", {
-    tbl <- "This is sample, so not tibble."
-    bin_dir <- "d:/pf/mecab"
-    option <- "some options"
-    if(stringr::str_detect(Sys.getenv(c("OS")), "Windows")){
-        exp <- "echo This is sample, so not tibble. \\|d:\\pf\\mecab\\mecab -b 120 some options"
-    } else {
-        exp <- "echo This is sample, so not tibble. |d:/pf/mecab/mecab -b 120 some options"
-    }
-    expect_equal(make_cmd_mecab(tbl, bin_dir, option), exp)
-
-    bin_dir <- "d:/pf/mecab/"
-    option <- NULL
-    if(stringr::str_detect(Sys.getenv(c("OS")), "Windows")){
-        exp <- "echo This is sample, so not tibble. \\|d:\\pf\\mecab\\mecab -b 120 "
-    } else {
-        exp <- "echo This is sample, so not tibble. |d:/pf/mecab/mecab -b 120 "
-    }
-    expect_equal(make_cmd_mecab(tbl, bin_dir, option), exp)
-
 })
