e94cb6c010774e0762c11160dbfc1921bea69312
Toshikazu Matsumura <matutosi@gmail.com>
2023-01-29 05:47:19 +0900

    update review code and data

--- data-raw/review.R
+++ data-raw/review.R
@@ -1,6 +1,7 @@
   # usethis::use_data_raw("review_sudachi")
 ## code to prepare `review` and related dataset go here
 library(tidyverse)
+library(rvest)
 devtools::load_all(".")
 
 review <-
@@ -44,13 +45,49 @@ usethis::use_data(review_sudachi_a, overwrite = TRUE)
 usethis::use_data(review_sudachi_b, overwrite = TRUE)
 usethis::use_data(review_sudachi_c, overwrite = TRUE)
 
-  # review_ginza
-  # tail(review_ginza) %>% unescape_utf()
+  # mecab
+bin_dir <- ""
+iconv   <- ""
+method  <- "chamame"
+review_chamame <- gen_morana_data(review, bin_dir = bin_dir, iconv = iconv, method = method)
+usethis::use_data(review_chamame, overwrite = TRUE)
+
+## for check
+  # tail(review_mecab    ) %>% unescape_utf()
+  # tail(review_sudachi_a) %>% unescape_utf()
+  # tail(review_sudachi_b) %>% unescape_utf()
+  # tail(review_sudachi_c) %>% unescape_utf()
+  # tail(review_ginza    ) %>% unescape_utf()
+  # tail(review_chamame  ) %>% unescape_utf()
+  # 
+  # review_mecab     %>% unescape_utf() %>% print(n=200)
+  # review_sudachi_a %>% unescape_utf() %>% print(n=200)
+  # review_ginza     %>% unescape_utf() %>% print(n=200)
+  # review_chamame   %>% unescape_utf() %>% print(n=200)
 
 ## code to prepare `review_chamame` dataset goes here
+prepare_chamame <- function(df){
+  cols <- 
+    c("\\u8f9e\\u66f8", "\\u6587\\u5883\\u754c", 
+      "\\u66f8\\u5b57\\u5f62\\uff08\\uff1d\\u8868\\u5c64\\u5f62\\uff09", 
+      "\\u8a9e\\u5f59\\u7d20", "\\u8a9e\\u5f59\\u7d20\\u8aad\\u307f", "\\u54c1\\u8a5e", 
+      "\\u6d3b\\u7528\\u578b", "\\u6d3b\\u7528\\u5f62", 
+      "\\u767a\\u97f3\\u5f62\\u51fa\\u73fe\\u5f62", "\\u4eee\\u540d\\u5f62\\u51fa\\u73fe\\u5f62", 
+      "\\u8a9e\\u7a2e", "\\u66f8\\u5b57\\u5f62(\\u57fa\\u672c\\u5f62)", 
+      "\\u8a9e\\u5f62(\\u57fa\\u672c\\u5f62)", "...14") %>% unescape_utf()
+  cols_index <- c(3,12,6)
+  new_cols <- 
+    c("\\u8868\\u5c64\\u5f62", "\\u539f\\u5f62", "\\u54c1\\u8a5e", 
+      stringr::str_c("\\u54c1\\u8a5e\\u518d\\u5206\\u985e", 1:3)) %>% unescape_utf()
+  df %>%
+    dplyr::select(all_of(cols[cols_index])) %>%
+    magrittr::set_colnames(new_cols[1:3]) %>%
+    tidyr::separate(new_cols[3], sep="-", into = new_cols[3:6], fill = "right", extra = "drop")
+}
+
 review %>%
   unescape_utf() %>%
-  readr::write_tsv("tools/pre_chamame_review.txt", col_names = FALSE)
+  readr::write_tsv("data-raw/pre_chamame_review.txt", col_names = FALSE)
 
   #  #   #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  # 
   # 
@@ -61,18 +98,7 @@ review %>%
   #  #   #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  # 
 
 review_chamame <-
-  readr::read_csv("tools/pre_chamame_review.csv") %>%
-  escape_utf() %>%
+  readr::read_csv("data-raw/pre_chamame_review.csv") %>%
+  prepare_chamame() %>%
+  escape_utf()
 usethis::use_data(review_chamame, overwrite = TRUE)
-
-
-## for check
-  # tail(review_mecab    )
-  # tail(review_sudachi_a)
-  # tail(review_sudachi_b)
-  # tail(review_sudachi_c)
-  # tail(review_ginza    )
-  # 
-  # review_mecab     %>% unescape_utf() %>% print(n=200)
-  # review_sudachi_a %>% unescape_utf() %>% print(n=200)
-  # review_ginza     %>% unescape_utf() %>% print(n=200)
