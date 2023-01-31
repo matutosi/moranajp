bc4b7ceb53f9a36b7d34aea42c3f81c63daf20eb
Toshikazu Matsumura <matutosi@gmail.com>
2023-02-01 04:52:45 +0900

    remove unused function

--- tests/testthat/test-moranajp.R
+++ tests/testthat/test-moranajp.R
@@ -1,14 +1,10 @@
-test_that("add_series_no and add_text_id work", {
+test_that("add_text_id() work", {
     times <- 2:4
     tbl <-
-      tibble::tibble(col=c(rep("a", times[1]), "EOS", rep("b", times[2]), "EOS", rep("c", times[3]), "EOS"), col_na=NA)
-    res <- c(rep(1, times[1] + 1), rep(2, times[2] + 1), rep(3, times[3] + 1))       # "+ 1": because of EOS
-    res_false <- c(rep(0, times[1]), rep(1, times[2] + 1), rep(2, times[3] + 1), 3)  # when separator indicate start
-      # add_series_no()
-    expect_equal(add_series_no(tbl, cond=".$col == 'EOS' & !is.na(.$col_na)")$series_no, rep(1, nrow(tbl)))
-    expect_equal(add_series_no(tbl, cond=".$col == 'EOS' &  is.na(.$col_na)")$series_no, res)
+      tibble::tibble(col=c(rep("a", times[1]), "EOS", rep("b", times[2]), "EOS", rep("c", times[3]), "EOS"))
+    res <- c(rep(1, times[1]), rep(2, times[2] + 1), rep(3, times[3] + 1), 4)       # "+ 1": because of EOS
       # add_text_id()
-    expect_equal(add_text_id(tbl)$text_id, res)
+    expect_equal(add_text_id(tbl, method = "mecab", brk = "EOS")$text_id, res)
 })
 
 test_that("text_id of moranajp matches text number", {
