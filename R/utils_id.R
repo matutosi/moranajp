ad2adc618d39d0c430f1cc5c594065fccab7f433
Toshikazu Matsumura <matutosi@gmail.com>
2024-09-03 15:43:23 +0900

    refactoring id functions

--- R/utils_id.R
+++ R/utils_id.R
@@ -1,35 +1,3 @@
-#' Add ids.
-#' 
-#' @param x              A string vector.
-#' @param brk            A string to specify the break between ids.
-#' @param col            A string to specify the column.
-#' @param df             A dataframe.
-#' @param end_with_brk   A logical.
-#'                       TRUE: brk means the end of groups.
-#'                       FALSE: brk means the beginning of groups.
-#' 
-#' @return  id_with_break() returns id vector, add_id_df() returns dataframe.
-#' @examples
-#' tmp <- c("a", "brk", "b", "brk", "c")
-#' brk <- "brk"
-#' text_id_with_break(tmp, brk)
-#' add_text_id_df(tibble::tibble(tmp), col = "tmp", "brk")
-#' 
-#' @export
-text_id_with_break <- function(x, brk, end_with_brk = TRUE){
-  text_id <- purrr::accumulate(x == brk, sum) + 1
-  if(end_with_brk) text_id <- c(1, text_id)[-length(text_id)]
-  return(text_id)
-}
-
-#' @rdname text_id_with_break
-#' @export
-add_text_id_df <- function(df, col, brk, end_with_brk = TRUE){
-  x <- df[[col]]
-  text_id <- text_id_with_break(x, brk, end_with_brk)
-  dplyr::bind_cols(df, text_id = text_id)
-}
-
 #' Add group id column into result of morphological analysis
 #' 
 #' @param tbl           A dataframe
@@ -48,26 +16,27 @@ add_text_id_df <- function(df, col, brk, end_with_brk = TRUE){
 #' @export
 add_group <- function(tbl, col, brk = "EOS", grp = "group", 
                       cond = NULL, end_with_brk = TRUE){
-  # col = "col"; brk = "EOS"; grp = "group"; cond = NULL; end_with_brk = TRUE; tbl <- tibble::tibble(col=c(rep("a", 2), brk, rep("b", 3), brk, rep("c", 4), brk))
-  adj <- "adjust"
+  # col = "col"; brk = "EOS"; grp = "group"; cond = NULL; end_with_brk = TRUEr
+  # tbl <- tibble::tibble(col = c(rep("a", 2), brk, rep("b", 3), brk, rep("c", 4), brk))
   if(is.null(cond)){
     cond <- paste0("tbl$", col, " == '", brk, "'")  # cond: .$col == 'brk'
   }
   tbl <- 
-    tbl |>
-    dplyr::mutate(`:=`({{grp}}, 
-      (eval(str2expression(cond))) + 0 )) |>  # "+ 0": boolean to numeric
-    dplyr::mutate(`:=`({{grp}}, purrr::accumulate(.data[[grp]], `+`))) |>
-    dplyr::mutate(`:=`({{grp}}, .data[[grp]] + 1))
-
+    dplyr::mutate(tbl, `:=`({{ grp }}, 
+                            cond |>
+                              eval_str() |> 
+                              cumsum()   |> 
+                              `+`(e1 = _, e2 = 1) ))
   if(end_with_brk){
     tbl <- 
-      tbl |>
-      dplyr::mutate(`:=`({{adj}}, -(.data[[col]] == brk) + 0 )) |>
-      dplyr::mutate(`:=`({{grp}}, .data[[grp]] + .data[[adj]])) |>
-      dplyr::select(-dplyr::all_of(adj))
+      dplyr::mutate(tbl, `:=`({{ grp }}, 
+                              dplyr::lag(tbl[[grp]], n = 1, default = 1)))
   }
   return(tbl)
+}
+
+eval_str <- function(str){
+  str |> str2expression() |> eval()
 }
 
 #' Wrapper function for add_group() to add sentence id
@@ -99,7 +68,7 @@ add_sentence_no <- function(df, s_id = "sentence"){
   df <- 
     df |>
     dplyr::mutate(`:=`({{s_id}}, 
-      (eval(str2expression(cond))) + 0 )) |>  # "+ 0": boolean to numeric
+      eval_str(cond) + 0 )) |>  # "+ 0": boolean to numeric
     dplyr::mutate(`:=`({{s_id}}, purrr::accumulate(.data[[s_id]], `+`))) |>
     dplyr::mutate(`:=`({{s_id}}, .data[[s_id]] + 1))
 
@@ -107,7 +76,7 @@ add_sentence_no <- function(df, s_id = "sentence"){
   df <- 
     df |>
     dplyr::mutate(`:=`({{adj}}, 
-      -(eval(str2expression(cond))) + 0 )) |>  # "+ 0": boolean to numeric
+      -eval_str(cond) + 0 )) |>  # "+ 0": boolean to numeric
     dplyr::mutate(`:=`({{s_id}}, .data[[s_id]] + .data[[adj]])) |>
     dplyr::select(-dplyr::all_of(adj))
 
